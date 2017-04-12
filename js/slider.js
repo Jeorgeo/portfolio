d2bc721db91b54dedede3b8aef721323cbd4eea8
Yury_Shakhouski <Jeorgeo@list.ru>
2017-04-12 13:57:08 +0300

    170412
    Подготовлены сжатые файлы html и css

--- js/slider.js
+++ js/slider.js
@@ -8,32 +8,33 @@
 
    $(document).ready(function(){
      $('.portfolio-cards').slick({
- dots: true,
- infinite: false,
- speed: 300,
- slidesToShow: 3,
- slidesToScroll: 1,
- responsive: [
-   {
-     breakpoint: 900,
-     settings: {
-       slidesToShow: 2,
+       dots: true,
+       infinite: false,
+       speed: 300,
+       slidesToShow: 3,
        slidesToScroll: 1,
-       infinite: true,
-       dots: true
-     }
-   },
-   {
-     breakpoint: 600,
-     settings: {
-       slidesToShow: 1,
-       slidesToScroll: 1
-     }
-   }
+       responsive: [
+         {
+           breakpoint: 900,
+           settings: {
+             slidesToShow: 2,
+             slidesToScroll: 1,
+             infinite: true,
+             dots: true
+           }
+         },
+         {
+           breakpoint: 600,
+           settings: {
+             slidesToShow: 1,
+             slidesToScroll: 1
+           }
+         }
 
    // You can unslick at a given breakpoint now by adding:
    // settings: "unslick"
    // instead of a settings object
- ]
+
+       ]
      });
    });
